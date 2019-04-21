8d21b466f105627091a6206170095bbe8e94af79
Zach Cervi <zachcervi@gmail.com>
2019-04-20 22:26:23 -0600

    Added CRUD for Movies API

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies')
 const app = express();
 const mongoose = require('mongoose');
 
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
-
+app.use('/api/movies', movies)
 mongoose
 	.connect('mongodb://localhost/Vidly')
 	.then(() => console.log('Connecting to MongoDb...'))
