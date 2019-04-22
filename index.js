ed2caf2272190b638bd8fbbc59b24c6e56365996
Zach Cervi <zachcervi@gmail.com>
2019-04-22 12:12:30 -0600

    Added rental CRUD and refactored code to validate object id

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
-const movies = require('./routes/movies')
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 const app = express();
 const mongoose = require('mongoose');
 
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
-app.use('/api/movies', movies)
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 mongoose
 	.connect('mongodb://localhost/Vidly')
 	.then(() => console.log('Connecting to MongoDb...'))
